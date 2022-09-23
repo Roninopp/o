b97b712ac7d73e24de8a1d8cdefbb4f77437b30a
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-09-23 21:58:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def is_admins(chat_id: int):
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
-        await message.reply_text("Hi! My name is Ishi. I'm an Artificial Intelligence\n /chatbot - [on|off]")
+        await message.reply_text("Hi! My name is Fanastic `management+fun+music` if need talk with me do\n /chatbot - [on|off]")
 
 
 @bot.on_message(
